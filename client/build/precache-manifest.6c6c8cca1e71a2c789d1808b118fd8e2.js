103d70bfaa67e406f6d9e49cb934e69abb783800
rickiangela <rickiangela@gmail.com>
2019-05-10 23:15:49 -0400

    sjn

---
+++ client/build/precache-manifest.6c6c8cca1e71a2c789d1808b118fd8e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f06bb918a189a0f5b4f006c234bb5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f872918b2308a958459a",
+    "url": "/static/css/main.2003e9b8.chunk.css"
+  },
+  {
+    "revision": "646cf5e49c6c95d1b9e3",
+    "url": "/static/js/2.d7065ee9.chunk.js"
+  },
+  {
+    "revision": "f872918b2308a958459a",
+    "url": "/static/js/main.cb9efcd3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
